666fe928e19ed89fe478cf68d491aabdd595ff6f
Neemias Calebe <neemiascalebe2014@gmail.com>
2025-06-07 16:14:55 -0300

    Remover importações não utilizadas e atualizar requisitos do Backend e Model, além de adicionar novos datasets para treinamento e teste

--- Backend/__init__.py
+++
@@ -1,2 +0,0 @@
-from saved_models import rf_undersampling_model
-from utils import loadModel
--- Frontend/cc-fraud-detector/src/features/home/types/index.ts
+++ Frontend/cc-fraud-detector/src/features/home/types/index.ts
@@ -6,10 +6,11 @@ export interface Dataset {
   }
 }
 
+export type Model = "rf-under" | "rf-over" | "lgbm" | "xgb";
+
 export interface DatasetsResponse {
   datasets: { id: number; label: string }[];
 }
-
 
 export interface DatasetOption {
   label: string;
@@ -18,7 +19,13 @@ export interface DatasetOption {
 export type DatasetOptions = DatasetOption[];
 
 export interface PredictionResponse {
-  prediction: 0
+  model_name: string
+  model_type: string
+  accuracy: number
+  precision: number
+  recall: number
+  f1_score: number
+  auc_roc: number
   fraud_probability: number
   is_fraud: boolean
 }
